485a3e14daf200c35b91bf29057abcba7fad8c24
ElamParithi Arul <mail@parithi.com>
2020-05-25 21:01:46 -0400

    Initial Commit

---
+++ Example/Pods/Target Support Files/ParithiBasics/ParithiBasics-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ParithiBasics : NSObject
+@end
+@implementation PodsDummy_ParithiBasics
+@end
---
+++ Example/Pods/Target Support Files/Pods-ParithiBasics_Tests/Pods-ParithiBasics_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ParithiBasics_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_ParithiBasics_Tests
+@end
